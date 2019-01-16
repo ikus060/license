12f330d0357ed00c31e556d29ecda8d3c071b53c
Patrik Dufresne <info@patrikdufresne.com>
2019-01-16 16:18:50 -0500

    Add manifest

--- src/main/java/com/patrikdufresne/license/Main.java
+++ src/main/java/com/patrikdufresne/license/Main.java
@@ -14,7 +14,6 @@ package com.patrikdufresne.license;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.text.ParseException;
